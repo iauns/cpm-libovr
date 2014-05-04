1553e1c5f6daff5b2d5b9c1082c0c8b979514ca3
James Hughes <jhughes@sci.utah.edu>
2014-05-04 11:36:11 -0600

    Initial commit of oculus libovr.

---
+++ tests/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "OVR.h"
+
+int main(int /*argc*/, char** /*argv*/)
+{
+  return 0;
+}
---
+++ tests/run-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+if [ ! -d ./bin ]; then
+  mkdir -p ./bin
+fi
+
+# Ensure we fail immediately if any command fails.
+set -e
+
+pushd ./bin > /dev/null
+  cmake ..
+  VERBOSE=1 make
+popd
+
+# Run the tests
+./bin/ovr_test
